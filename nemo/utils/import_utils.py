d12ba2f1469773750db1e957c99381da87a1b983
Ethan He <yihuih@nvidia.com>
2025-05-24 16:34:11 -0700

    Fix typo in GPU-only import message

--- nemo/utils/import_utils.py
+++ nemo/utils/import_utils.py
@@ -363,7 +363,7 @@ def gpu_only_import(module, *, alt=None):
 
     return safe_import(
         module,
-        msg=f"{module} is not enabled in non GPU-enabled installations or environemnts. {GPU_INSTALL_STRING}",
+        msg=f"{module} is not enabled in non GPU-enabled installations or environments. {GPU_INSTALL_STRING}",
         alt=alt,
     )
 
